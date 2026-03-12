8fc26d6615d40527ba662857c53b35b916eacfbe
a-monet <alexusmonetlovecalhoun@gmail.com>
2026-03-12 15:59:29 -0500

    modified comment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectMongo = require("./server/database/connect");
-const session = require('express-session'); // allows us to store session tokens (logging into Google Calendar) 
+const session = require('express-session'); // allows us to store session tokens
 const seedOpenCenter = require("./server/seed/seedOpenCenter");
 
 
